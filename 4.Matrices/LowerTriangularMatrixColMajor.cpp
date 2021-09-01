3e4f371e5a2eb2f1efee55a3c68de9e3f45f068e
Harshit <theoopsguy01@gmail.com>
2021-09-01 22:40:39 +0530

    Added Column major approach to lower triangular matrix

---
+++ 4.Matrices/LowerTriangularMatrixColMajor.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+using namespace std;
+
+class lowerTriangle
+{
+    private:
+        int size;
+        int *arr;
+
+    public:
+        lowerTriangle(int n)
+        {
+            this->size = n;
+            this->arr = new int[n*(n+1)/2];     
+            
+            //in a lower triangular matrix no. of nonzero elements is n(n+1)/2
+        }
+
+        void set(int row, int col, int data);
+
+        int get(int row, int col);
+
+        int getDimension()
+        {
+            return size;
+        }
+
+        void display();
+
+        ~lowerTriangle()
+        {
+            delete []arr;
+        }
+};
+
+void lowerTriangle::set(int row, int col, int data)
+{
+    if (row >= col)
+        arr[(size * (col - 1) - (col-1)*(col-2)/2) + row - col] = data;
+        //index of ele M[5][4] in our array will be (5 + 4 + 3) + 1 = 13
+}
+
+int lowerTriangle::get(int row, int col)
+{
+    if (row >= col)
+        return arr[(size * (col - 1) - (col-1)*(col-2)/2) + row - col];
+    else
+        return 0;
+}
+
+void lowerTriangle::display()
+{
+    for (int row = 1; row <= size; row++)
+    {
+        for (int col = 1; col <= size; col++)
+        {
+            if (row >= col)
+                cout<<arr[(size * (col - 1) - (col-1)*(col-2)/2) + row - col]<<" ";
+            else
+                cout<<"0 ";
+        }
+
+        cout<<endl;
+    }
+}
+
+int main()
+{
+    int size;
+    cout<<"Enter Dimensions: \n";
+    cin>>size;
+    lowerTriangle lt(size);
+    int data;
+    cout<<"Enter all the elements: \n";
+    for (int row = 1; row <= size; row++)
+    {
+        for (int col = 1; col <= size; col++)
+        {
+            cin>>data;
+            lt.set(row, col, data);
+        }
+    }
+    lt.display();
+    return 0;
+}
