315ebd96825faa76ecb9b0500f460998e29cb6cf
Harshit Jain <theoopsguy01@gmail.com>
2021-09-14 00:07:46 +0530

    Added Recursive code for merging 2 sorted lists

--- 5.LinkedList/MergeSortInLinkedList.cpp
+++ 5.LinkedList/MergeSortInLinkedList.cpp
@@ -51,124 +51,6 @@ Node *takeInput()
     return head;
 }
 
-int length(Node *head)
-{
-    int l = 0;
-    Node *temp = head;
-    while (temp)
-    {
-        temp = temp->next;
-        l++;
-    }
-    return l;
-}
-
-Node* Insert(Node *head, int idx, int x)
-{
-    Node *ptr = head;
-    
-    if (idx < 0 || idx > length(head))
-        return head;
-
-    Node *newNode = new Node (x);
-
-    if (idx == 0)
-    {
-        newNode->next = head;
-        head = newNode;
-        return head;
-    }
-
-    for (int pos = 0; pos < idx - 1 && ptr != NULL; pos++)
-    {
-        ptr = ptr->next;
-    }
-
-    if (ptr)
-    {
-        newNode->next = ptr->next;
-        ptr->next = newNode;
-    }
-
-    return head;
-}
-
-Node *InsertRec(Node *head, int idx, int data)
-{
-    if (head == NULL || idx < 0)
-        return head;
-
-    if (idx == 1)
-    { //idx == 1 coz we need to stop and do our work at the node before the idx where elements is to be inserted
-        Node *newNode = new Node(data);
-        newNode->next = head->next;
-        head->next = newNode;
-        return head;
-    }
-
-    if (idx == 0)
-    {//first element is to be added
-        Node *newNode = new Node(data);
-        newNode->next = head;
-        head = newNode;
-        return head;
-    }
-
-    Node *temp = InsertRec(head->next, idx - 1, data);
-    return head;
-}
-
-Node* deleteNode (Node *head, int idx)
-{
-    Node *temp = head;
-
-    if (idx < 0 || idx > length(head) || head == NULL)
-        return head;
-
-    if (idx == 0)
-    {
-        head = head->next;
-        delete temp;
-    }
-
-    else 
-    {
-        for (int pos = 0; pos < idx - 1; pos++)
-            temp = temp->next;
-
-        Node *del = temp->next;
-        temp->next = del->next;
-        delete del;
-    }
-
-    return head;
-}
-
-Node* DeleteRec (Node *head, int idx)
-{
-    if (head == NULL || idx < 0)
-        return head;
-
-    if (idx == 1)
-    {
-        Node *del = head->next;
-        head->next = del->next;
-        delete del;
-        return head;
-    }    
-
-    if (idx == 0)
-    {
-        Node *del = head;
-        head = head->next;
-        delete del;
-        return head;
-    }
-
-    Node *temp = DeleteRec(head->next, idx - 1);
-    return head;
-}
-
 Node* FindMiddle (Node *head)
 {
     Node *slow = head, *fast = head->next;
@@ -238,6 +120,28 @@ Node *merge (Node *head1, Node *head2)
     return fhead;
 }
 
+Node *mergeRec(Node *head1, Node *head2)
+{
+    Node *fhead = NULL;
+    
+    if (head1 == NULL)
+        return head2;
+    else if (head2 == NULL)
+        return head1;
+    
+    if (head1->data <= head2->data)
+    {
+        fhead = head1;
+        fhead->next = mergeRec(head1->next, head2);
+    }
+    else
+    {
+        fhead = head2;
+        fhead->next = mergeRec(head1, head2->next);
+    }
+    return fhead;
+}
+
 void mergeSort(Node **head)
 {
     Node *curr = *head, *first, *second;
@@ -254,7 +158,7 @@ void mergeSort(Node **head)
     mergeSort(&first);      //calling recursion on two halves
     mergeSort(&second);
 
-    *head = merge (first, second);  //merging the 2 sorted halves
+    *head = mergeRec(first, second);  //merging the 2 sorted halves
 }
 
 int main()
