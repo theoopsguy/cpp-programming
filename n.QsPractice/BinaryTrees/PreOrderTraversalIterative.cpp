1a09f45e470d780855c42fb556e652b8be6a9103
theoopsguy <theoopsguy01@gmail.com>
2022-07-15 18:37:48 +0530

    add preorder traversal iterative

---
+++ n.QsPractice/BinaryTrees/PreOrderTraversalIterative.cpp
@@ -0,0 +1,26 @@
+//https://leetcode.com/problems/binary-tree-preorder-traversal/
+
+// TC: O(n)
+// SC: O(n)
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if (!root)
+            return ans;
+        stack<TreeNode*> st;
+        st.push(root);
+
+        while(!st.empty()) {
+            TreeNode* node = st.top();
+            st.pop();
+            if (node->right)
+                st.push(node->right);
+            if (node->left)
+                st.push(node->left);
+            ans.push(node->val);
+        }
+        return ans;
+    }
+};
