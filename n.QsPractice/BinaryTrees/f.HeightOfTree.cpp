7f98c01c33c1cdf4852ca3e4c8335540dba3157e
theoopsguy <theoopsguy01@gmail.com>
2022-07-16 20:37:06 +0530

    Add height of binary Tree

---
+++ n.QsPractice/BinaryTrees/f.HeightOfTree.cpp
@@ -0,0 +1,20 @@
+// TC: O(n)
+// SC: O(n)
+// Recusive soln:
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (!root)
+            return 0;
+        
+        int lh = maxDepth(root->left);
+        int rh = maxDepth(root->right);
+        
+        return (max(lh, rh) + 1);
+    }
+};
+
+//Iterative can be to do level order traversal and have a variable which increments when we go left or right,
+// and the highest level will be the ans.
+// Same complexity.
